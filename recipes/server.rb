119192426c42c7ccbed33dfa751e1af5337492fa
osawagiboy <osawagiboy@gmail.com>
2014-08-18 06:00:11 +0900

    small fix

--- recipes/server.rb
+++ recipes/server.rb
@@ -51,10 +51,11 @@ end
 include_recipe 'munin::client'
 
 sysadmins = []
+data_bag = node['munin']['data_bag']
 if Chef::Config[:solo]
-  sysadmins = data_bag('users').map { |user| data_bag_item('users', user) }
+  sysadmins = data_bag(data_bag).map { |user| data_bag_item(data_bag, user) }
 else
-  sysadmins = search(:users, 'groups:sysadmin')
+  sysadmins = search(data_bag, 'groups:sysadmin')
 end
 
 if Chef::Config[:solo]
